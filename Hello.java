08f6343c0bd88b12b40600fe7e9893cdf92b8e66
rasnerge <54372863+rasnerge@users.noreply.github.com>
2019-08-21 14:02:24 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello  {
+ public static void printHeading()  {
+  System.out.println("Gavin");
+  System.out.println("Hello.java");
+  System.out.println("firstcode");
+  System.out.println("1 8/21/2019");
+  System.out.println("255 002");
+  System.out.println();
+ }
+ 
+ public static void main(String [] args) {
+  printHeading();
+  System.out.println("Hello");
+ }
+} 
